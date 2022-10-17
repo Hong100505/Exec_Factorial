96be6ea9a79074a4fe1e67ea211669ee9ba88bdd
ispan <ispan@DESKTOP-PAKSETB>
2022-10-17 16:55:25 +0800

    加入專案檔案。

---
+++ Exec_Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int value = 1;
+			int numberInt = 7;
+
+			for (int i = 1; i <= numberInt; i++)
+			{
+				value = value * i;
+			}
+			Console.WriteLine("階乘" + numberInt + "等於" + value);
+		}
+	}
+}
